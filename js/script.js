cd0bf138f66c6bd42b57a0edbd6c182232e278ec
ahyaralfaruq <ahyaralfaruq97@gmail.com>
2023-07-24 18:31:32 +0700

    changing card style in my skills section

--- js/script.js
+++ js/script.js
@@ -173,9 +173,8 @@ document.addEventListener("DOMContentLoaded", function() {
 
 			getItems.forEach(function(item) {
 				item.style.transform = `translateX(-${width * index}%)`
-				console.log(width * index)
 			})
-			console.log(index)
+			isTransition = true
 			currentIndex = index
 
 		}
@@ -196,6 +195,98 @@ document.addEventListener("DOMContentLoaded", function() {
 		prevBtn.addEventListener("click", prev)
 	}
 
+	function snowingEffect() {
+		const test = document.querySelector(".snowing-wrapper")
+		let snowmax = 50
+		let snowcolor = ["#aaaacc","#ddddFF","#ccccDD"]
+		let snowtype = ["Arial Black","Arial Narrow","Times","Comic Sans MS"]
+		let snowletter = "*"
+		let sinkspeed = 0.6
+		let snowmaxsize = 22
+		let snowminsize = 8
+		let snowingzone = 1
+		let snow = []
+		let marginbottom
+		let marginright
+		let timer
+		let i_snow = 0
+		let x_mv = []
+		let crds = []
+		let lftrght = []
+		const browserinfos = navigator.userAgent 
+		const ie5 = document.all&&document.getElementById&&!browserinfos.match(/Opera/)
+		const ns6 = document.getElementById&&!document.all
+		const opera = browserinfos.match(/Opera/)  
+		const browserok = ie5||ns6||opera
+
+		function randommaker(range) {        
+			const rand = Math.floor(range*Math.random())
+			return rand
+		}
+		
+		function movesnow() {
+			for(let i = 0; i <= snowmax; i++) {
+				crds[i] += x_mv[i]
+				snow[i].posy += snow[i].sink
+				snow[i].style.left = snow[i].posx + lftrght[i] * Math.sin(crds[i])
+				snow[i].style.top = snow[i].posy
+					
+				if (snow[i].posy >= marginbottom - 2 * snow[i].size || parseInt(snow[i].style.left) >(marginright - 3 * lftrght[i])){
+					if (snowingzone == 1) snow[i].posx = randommaker(marginright - snow[i].size)
+					if (snowingzone == 2) snow[i].posx = randommaker(marginright / 2-snow[i].size)
+					if (snowingzone == 3) snow[i].posx = randommaker(marginright / 2-snow[i].size) + marginright / 4
+					if (snowingzone == 4) snow[i].posx = randommaker(marginright / 2-snow[i].size) + marginright / 2
+					snow[i].posy = 0
+				}
+			}
+			timer = setTimeout(movesnow, 30)
+		}
+
+		function initsnow() {
+			if (ie5 || opera) {
+				marginbottom = document.body.clientHeight
+				marginright = document.body.clientWidth
+			} else if (ns6) {
+				marginbottom = window.innerHeight
+				marginright = window.innerWidth
+			}
+			
+			let snowsizerange = snowmaxsize-snowminsize
+			for(let i = 0; i <= snowmax; i++) {
+				crds[i] = 0                    
+				lftrght[i] = Math.random() * 15
+				x_mv[i] = 0.03 + Math.random() / 10
+				snow[i] = document.getElementById("s" + i)
+				snow[i].style.fontFamily = snowtype[randommaker(snowtype.length)]
+				snow[i].size = randommaker(snowsizerange) + snowminsize
+				snow[i].style.fontSize = snow[i].size
+				snow[i].style.color = snowcolor[randommaker(snowcolor.length)]
+				snow[i].sink = sinkspeed * snow[i].size / 5
+
+				if (snowingzone == 1) snow[i].posx = randommaker(marginright-snow[i].size)
+				if (snowingzone == 2) snow[i].posx = randommaker(marginright / 2 - snow[i].size)
+				if (snowingzone == 3) snow[i].posx = randommaker(marginright / 2 - snow[i].size) + marginright / 4
+				if (snowingzone == 4) snow[i].posx = randommaker(marginright / 2 - snow[i].size) + marginright / 2
+
+				snow[i].posy = randommaker(2 * marginbottom - marginbottom - 2 * snow[i].size)
+				snow[i].style.left = snow[i].posx
+				snow[i].style.top = snow[i].posy
+			}
+			movesnow()
+		}
+
+
+		for(let i = 0; i <= snowmax; i++) {
+			// document.write(`<span id="s${i}" style="position: absolute; top: -${snowmaxsize}">${snowletter}</span>`)
+			test.innerHTML = `<span id="s${i}" style="position: absolute; top: -${snowmaxsize}">${snowletter}</span>`
+		}
+
+		// if (browserok) {
+		// 	window.onload=initsnow
+		// }
+		initsnow()
+	}
+
 	// function waypointByFunction() {
 
 	// 	function waypointFunction(classes, direction = "down", callback, offset) {
@@ -208,4 +299,5 @@ document.addEventListener("DOMContentLoaded", function() {
 	toggleMenu()
 	clickSeeMoreDescription()
 	SliderCards()
+	// snowingEffect()
 })
